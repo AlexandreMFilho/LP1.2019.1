8a04711b8156bc7fbbe32d8da9a6ea6d8fee4866
Alexandre Filho <45580663+AlexandreMFilho@users.noreply.github.com>
2019-05-07 09:29:16 -0300

    Create Exercício_U3.1_Alexandre_Filho.c

---
+++ Exercício_U3.1_Alexandre_Filho.c
@@ -0,0 +1,15 @@
+/*
+Exercício U3.1 - Sequências de escape
+Número máximo de arquivos: 1
+Tipo de trabalho: Trabalho individual
+Exercício U3.1 - Sequências de escape
+Escreva a função escape capaz de converter os códigos ascii dos caracteres 
+{' ','\t', '\f','\v','\n','\r'}  quebrando-os  em dois caracteres '\\' + 'X'. 
+
+OBS.:  
+    Não é permitido o uso de if.
+    É obrigatório modularizar o código em três funções: uma para a leitura da
+    sequência original, a segunda para a conversão e a terceira para fazer a 
+    impressão da string final.
+    */
+    
