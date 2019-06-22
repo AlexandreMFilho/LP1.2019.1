ede54c931df58afa29b3420f48154981d0951036
Alexandre Filho <45580663+AlexandreMFilho@users.noreply.github.com>
2019-06-22 15:56:45 -0300

    Create Exercício_U4.1_Alexandre_Filho.c

---
+++ Exercício_U4.1_Alexandre_Filho.c
@@ -0,0 +1,11 @@
+Exercício U4.1 - Análise de arquivo texto
+Número máximo de arquivos: 1
+Tipo de trabalho: Trabalho individual
+Exercício U4.1 - Leitura e análise de arquivo texto
+Faça um programa que conte o número de caracteres imprimíveis, o número de linhas e o número total de caracteres de um arquivo. O nome do arquivo deve ser fornecido pelo teclado.
+
+Saída fornecida pelo seu programa:
+
+- Caso ocorra erro de arquivo, o programa deve imprimir: "Erro no arquivo %s\n" onde %s é substituído pelo nome do arquivo tal qual fornecido pelo teclado.
+
+- Caso o arquivo de entrada esteja disponível, o programa deve imprimir: Numero de caracteres lidos + \n + número de caracteres imprimíveis lidos + \n + número de linhas + \n
