0fb320b361108b2c196f49d8cf12048232476e41
Alexandre Filho <45580663+AlexandreMFilho@users.noreply.github.com>
2019-06-22 15:57:21 -0300

    Create Exercício_U4.2_Alexandre_Filho.c

---
+++ Exercício_U4.2_Alexandre_Filho.c
@@ -0,0 +1,15 @@
+Exercício U4.2 - Leitura de Arquivo imagem PGM P2
+Número máximo de arquivos: 1
+Tipo de trabalho: Trabalho individual
+Exercício U4.2 - Leitura de Arquivo texto
+Faça um programa que leia um arquivo de imagem formato PGM variante P2 e imprima os valores dos pixels em hexadecimal. Caso o arquivo não seja encontrado, seu programa deve escrever no canal de erro a string "Erro: arquivo inexistente\n" e sua execução deve ser interrompida.
+
+Instruções para a impressão:
+
+1- Cada linha da imagem deve ser impressa em uma linha de texto
+
+2 - Cada pixel impresso deve ocupar três caracteres de largura.
+
+3 - Dois pixels seguidos impressos devem ser separados por um caracter espaço
+
+4 - O último pixel da linha de texto NÃO deve ser sucedido por um caracter espaço
