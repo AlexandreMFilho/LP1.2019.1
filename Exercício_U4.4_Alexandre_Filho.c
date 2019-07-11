aefe2b79da1b3dc36603a2c86224f2d6adfbc34f
Alexandre Filho <45580663+AlexandreMFilho@users.noreply.github.com>
2019-07-11 19:42:33 -0300

    Update Exercício_U4.4_Alexandre_Filho.c

--- Exercício_U4.4_Alexandre_Filho.c
+++ Exercício_U4.4_Alexandre_Filho.c
@@ -1,1 +1,6 @@
-
+Exercício U4.4 - Formatação em 80 colunas
+Número máximo de arquivos: 1
+Tamanho máximo de arquivo carregado: 256 KiB
+Tipo de trabalho: Trabalho individual
+Exercício U4.4 - Formatação em 80 colunas
+Escreva um programa que leia um texto com largura (quantidade de colunas) ilimitada e produza na tela uma versão deste mesmo texto com largura máxima de 80 colunas. Atenção: as palavra não podem ser quebradas em mais de uma linha. A cada nova palavra lida deve ser avaliado se esta fará com que a linha atual exceda 80 colunas, quando isto ocorrer, deve se incluído um caracter '\n' antes dela.
