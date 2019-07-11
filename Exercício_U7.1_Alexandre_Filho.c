df01abc21bbbcc0cdb7f0da4f58745857dcfc84b
Alexandre Filho <45580663+AlexandreMFilho@users.noreply.github.com>
2019-07-11 19:45:52 -0300

    Update Exercício_U7.1_Alexandre_Filho.c

--- Exercício_U7.1_Alexandre_Filho.c
+++ Exercício_U7.1_Alexandre_Filho.c
@@ -1,1 +1,10 @@
+Exercício U7.1 - Passagem de parâmetros por referência
+Número máximo de arquivos: 1
+Tipo de trabalho: Trabalho individual
+Exercício U7.1 - Passagem de parâmetros por referência
+Escreva um programa que receba* dois números ponto flutuante por stdin. Estes números são analisados e o programa deve imprimir a seguinte saída:
+o maior dos números + '\n' + o menor número + '\n'.
 
+Seu programa deve ser estruturado da seguinte forma: Ele terá duas funções. A primeira, função swap, troca os valores de seus argumentos passados como entrada. A segunda função, chamada maxmin, recebe dois argumentos do tipo float, max e min.  Independentemente que quem seja o maior dos parâmetros no momento da chamada da função, ao final da execução de maxmin, max deve conter o maior dos argumentos e min o menor. Para isto, quando necessário maxmin deve chamar a função swap()
+
+* não imprima mensagens na tela pedindo a entrada dos números
