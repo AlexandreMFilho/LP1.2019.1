06caebc73d81a67ea85b909c9b4469d9bb67486b
Alexandre Filho <45580663+AlexandreMFilho@users.noreply.github.com>
2019-07-11 19:44:52 -0300

    Update Exercício_U5.4_Alexandre_Filho.c

--- Exercício_U5.4_Alexandre_Filho.c
+++ Exercício_U5.4_Alexandre_Filho.c
@@ -1,1 +1,5 @@
-
+Exercício U5.4 - Conversão de inteiro decimal para octal
+Número máximo de arquivos: 1
+Tipo de trabalho: Trabalho individual
+Exercício U5.4 - Conversão de inteiro decimal para octal
+Escreva uma solução recursiva para a impressão do valor octal correspondente ao numero decimal fornecido como entrada.
