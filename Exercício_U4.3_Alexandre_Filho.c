232540cfbe5f267008a75601416f10f7414b1623
Alexandre Filho <45580663+AlexandreMFilho@users.noreply.github.com>
2019-07-11 19:41:30 -0300

    Update Exercício_U4.3_Alexandre_Filho.c

--- Exercício_U4.3_Alexandre_Filho.c
+++ Exercício_U4.3_Alexandre_Filho.c
@@ -1,1 +1,37 @@
+/*
+Exercício U4.3 - Leitura de arquivo binário
+Número máximo de arquivos: 1
+Tipo de trabalho: Trabalho individual
+Exercício U4.3 - Leitura de arquivo binário
+Faça um programa que leia um arquivo de imagem no formato PGM na variante P5 e apresente na tela do terminal  o conteúdo correspondente ao formato PGM variante P2. Caso o arquivo não seja encontrado, seu programa deve escrever no canal de erro a string "Erro: arquivo inexistente\n" e sua execução deve ser interrompida.
 
+Instruções para a impressão:
+
+1- Cada linha da imagem deve ser impressa em uma linha de texto
+
+2 - Cada pixel impresso deve ocupar três caracteres de largura.
+
+3 - Dois pixels seguidos impressos devem ser separados por um caracter espaço
+
+4 - O último pixel da linha de texto NÃO deve ser sucedido por um caracter espaço
+
+Links úteis sobre a variantes do formato de imagem pgm:http://en.wikipedia.org/wiki/Netpbm_format#PGM_example
+http://netpbm.sourceforge.net/doc/pgm.html
+
+Exemplo de resultado da análise do arquivo
+
+P2
+# A diagonal line 10 pixels wide and 10 pixels high
+10 10
+255
+  0 255 255 255 255 255 255 255 255 255
+255   0 255 255 255 255 255 255 255 255
+255 255   0 255 255 255 255 255 255 255
+255 255 255   0 255 255 255 255 255 255
+255 255 255 255   0 255 255 255 255 255
+255 255 255 255 255   0 255 255 255 255
+255 255 255 255 255 255   0 255 255 255
+255 255 255 255 255 255 255   0 255 255
+255 255 255 255 255 255 255 255   0 255
+255 255 255 255 255 255 255 255 255   0
+/*
